caaaf36e6bac8c2a374b2d5b322076e2600bcf12
Hunter Pollock <hunter@hnt8.net>
2024-02-08 15:33:44 -0500

    Init window

---
+++ SCPCBR/GameAssembly.h
@@ -0,0 +1,8 @@
+#ifndef GAMEASSEMBLY_H
+#define GAMEASSEMBLY_H
+
+#define GameTitle "SCP: Containment Breach Remastered"
+#define GameVersion "v2.0.0"
+#define GameWindowTitle GameTitle " " GameVersion
+
+#endif // GAMEASSEMBLY_H
--- SCPCBR/SCPCBR.cpp
+++ SCPCBR/SCPCBR.cpp
@@ -1,4 +1,35 @@
+#pragma comment(lib, "glfw3dll.lib")
+#include <GLFW/glfw3.h>
+
+#include "GameAssembly.h"
+
 int main(int argc, char* argv[])
 {
+    if (!glfwInit()) {
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    
+    GLFWwindow* window = glfwCreateWindow(640, 480, GameWindowTitle, NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(0);
+
+    while (!glfwWindowShouldClose(window)) {
+        // TODO: Render here
+        
+        //glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
