b1e255c6adc561f806f0290282863ac257694eb5
Hunter Pollock <hunter@hnt8.net>
2024-02-10 13:43:15 -0500

    Created the SteamWrapper class

---
+++ SCPCBR/SteamWrapper/SteamWrapper.cpp
@@ -0,0 +1,27 @@
+#include "SteamWrapper.h"
+
+#include "../Util/Util.h"
+
+#include <steam/steam_api.h>
+
+SteamWrapper::SteamWrapper() {
+    if (!SteamAPI_IsSteamRunning()) {
+        Util::Error::Exit("Please start Steam before attempting to open the game.");
+    }
+    
+    if (SteamAPI_RestartAppIfNecessary(2090230)) {
+        Util::Error::ExitFast();
+    }
+    
+    if (!SteamAPI_Init()) {
+        Util::Error::Exit("Failed to initialize the Steam API. Ensure you are launching the game through Steam!");
+    }
+}
+
+SteamWrapper::~SteamWrapper() {
+    SteamAPI_Shutdown();
+}
+
+void SteamWrapper::RunCallbacks() {
+    SteamAPI_RunCallbacks();
+}
---
+++ SCPCBR/SteamWrapper/SteamWrapper.h
@@ -0,0 +1,14 @@
+#ifndef STEAMWRAPPER_H
+#define STEAMWRAPPER_H
+
+class SteamWrapper {
+public:
+    SteamWrapper();
+    ~SteamWrapper();
+
+    void RunCallbacks();
+
+    void SetNotificationPosition()
+};
+
+#endif // STEAMWRAPPER_H
