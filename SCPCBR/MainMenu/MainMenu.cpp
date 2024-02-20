6cf33804765a9650fd0ec8d00d8b81bbeab195c5
Hunter Pollock <hunter@hnt8.net>
2024-02-20 18:41:45 -0500

    Classic SCP:CB Main Menu (Having issues with certain UI styling with ImGui, so I'm making the classic UI)

--- SCPCBR/MainMenu/MainMenu.cpp
+++ SCPCBR/MainMenu/MainMenu.cpp
@@ -4,7 +4,10 @@
 #include "../AudioEngine/AudioEngine.h"
 
 #include <imgui/imgui.h>
+#include <imgui/imgui_custom.h>
 #include <FMOD/fmod.hpp>
+
+#include "../Localization/Localization.h"
 
 Util::Image::Image* mainMenuBackgroundImage;
 FMOD::Channel* mainMenuBackgroundMusic;
@@ -15,8 +18,8 @@ void MainMenu::Init() {
     mainMenuBackgroundImage = new Util::Image::Image;
 
     mainMenuBackgroundImage = new Util::Image::Image;
-    if (!Util::Image::LoadImageFromFile("Assets/GFX/Menu/MenuBackground.png", mainMenuBackgroundImage)) {
-        Util::Error::Exit("The texture \"Assets/GFX/Menu/MenuBackground.png\" failed to load. Ensure the file exists, or verify your game files.");
+    if (!Util::Image::LoadImageFromFile("Assets/GFX/Menu/MenuBackground.jpg", mainMenuBackgroundImage)) {
+        Util::Error::Exit("The texture \"Assets/GFX/Menu/MenuBackground.jpg\" failed to load. Ensure the file exists, or verify your game files.");
     }
 }
 
@@ -28,22 +31,57 @@ void MainMenu::Render(GLFWwindow* window, GlobalGameState* gameState) {
         AudioEngine::PlaySoundByName("Assets/SFX/Music/Menu.ogg", AudioEngine::GetChannelGroup("Music"));
         mainMenuStarted = true;
     }
+
+    std::string verString = "SCP: CONTAINMENT BREACH REMASTERED V2.0.0 ALPHA";
+    ImGui::GetForegroundDrawList()->AddText(ImVec2(1, height - ImGui::CalcTextSize(verString.c_str()).y - 1), ImColor(255, 255, 255), verString.c_str());
     
     // RENDER BACKGROUND
-    {
-        ImVec2 pos1;
-        ImVec2 pos2;
-
-        if (height * ((double)mainMenuBackgroundImage->Width / mainMenuBackgroundImage->Height) >= width) {
-            pos1 = ImVec2(0 - (height * ((double)mainMenuBackgroundImage->Width / mainMenuBackgroundImage->Height) - width) / 2, 0);
-            pos2 = ImVec2(width + (height * ((double)mainMenuBackgroundImage->Width / mainMenuBackgroundImage->Height) - width) / 2, height);
-        } else {
-            pos1 = ImVec2(0, 0 - (width / ((double)mainMenuBackgroundImage->Width / mainMenuBackgroundImage->Height) - height) / 2);
-            pos2 = ImVec2(width, height + (width / ((double)mainMenuBackgroundImage->Width / mainMenuBackgroundImage->Height) - height) / 2);
-        }
-
-        ImGui::GetBackgroundDrawList()->AddImage(reinterpret_cast<void*>(static_cast<intptr_t>(mainMenuBackgroundImage->TextureId)), pos1, pos2);
+    { 
+        ImGui::GetBackgroundDrawList()->AddImage(
+            reinterpret_cast<void*>(static_cast<intptr_t>(mainMenuBackgroundImage->TextureId)), ImVec2(0, 0),
+            ImVec2(mainMenuBackgroundImage->Width, mainMenuBackgroundImage->Height));
     }
+
+    // RENDER MENU
+    /*{
+        static int menu = 0;
+
+        switch (menu) {
+            case 0: {
+                ImGui::PushStyleColor(ImGuiCol_WindowBg, ImVec4(0, 0, 0, 0));
+                ImGui::PushStyleColor(ImGuiCol_Border, ImVec4(0, 0, 0, 0));
+                ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0, 0, 0, 0));
+                ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4(0, 0, 0, 0));
+                ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4(0, 0, 0, 0));
+                ImGui::SetNextWindowSize(ImVec2(700, 500));
+                ImGui::SetNextWindowPos(ImVec2(width / 2.0f, height / 2.0f), 0, ImVec2(0.5f, 0.5f));
+                ImGui::Begin("## MAINMENU-MENU", nullptr, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoMove);
+                
+                ImGui::PushFont(Localization::GetActiveLanguageCourierNewExtraLarge());
+                
+                if (ImGui::Button("NEW GAME", ImVec2(700, 80))) {
+                    
+                }
+
+                if (ImGui::Button("LOAD GAME", ImVec2(700, 80))) {
+                    
+                }
+
+                if (ImGui::Button("OPTIONS", ImVec2(700, 80))) {
+                    
+                }
+
+                if (ImGui::Button("QUIT", ImVec2(700, 80))) {
+                    
+                }
+                
+                ImGui::PopFont();
+
+                ImGui::End();
+                ImGui::PopStyleColor(5);
+            }
+        }
+    }*/
 }
 
 void MainMenu::Free() {
