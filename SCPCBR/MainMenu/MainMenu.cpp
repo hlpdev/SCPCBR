b6381d8bdd8a4e16b15dee0cd5f98b2d8ba216d9
Hunter Pollock <hunter@hnt8.net>
2024-02-22 12:11:22 -0500

    Main Menu, Not splitting this

--- SCPCBR/MainMenu/MainMenu.cpp
+++ SCPCBR/MainMenu/MainMenu.cpp
@@ -13,11 +13,10 @@
 Util::Image::Image* mainMenuBackgroundImage;
 FMOD::Channel* mainMenuBackgroundMusic;
 
-Util::Image::Image* newGameImage;
-Util::Image::Image* loadGameImage;
-Util::Image::Image* optionsImage;
-Util::Image::Image* quitImage;
-Util::Image::Image* backImage;
+Util::Image::Image* whiteImage;
+Util::Image::Image* blackImage;
+Util::Image::Image* scp173Image;
+Util::Image::Image* scpTextImage;
 
 bool mainMenuStarted = false;
 
@@ -30,149 +29,159 @@ void MainMenu::Init() {
         Util::Error::Exit("The texture \"Assets/GFX/Menu/MenuBackground.jpg\" failed to load. Ensure the file exists, or verify your game files.");
     }
 
-    newGameImage = new Util::Image::Image;
-    if (!Util::Image::LoadImageFromFile("Assets/GFX/Menu/NewGame.png", newGameImage)) {
-        Util::Error::Exit("The texture \"Assets/GFX/Menu/NewGame.png\" failed to load. Ensure the file exists, or verify your game files.");
+    whiteImage = new Util::Image::Image;
+    if (!Util::Image::LoadImageFromFile("Assets/GFX/Menu/White.png", whiteImage)) {
+        Util::Error::Exit("The texture \"Assets/GFX/Menu/White.png\" failed to load. Ensure the file exists, or verify your game files.");
     }
 
-    loadGameImage = new Util::Image::Image;
-    if (!Util::Image::LoadImageFromFile("Assets/GFX/Menu/LoadGame.png", loadGameImage)) {
-        Util::Error::Exit("The texture \"Assets/GFX/Menu/LoadGame.png\" failed to load. Ensure the file exists, or verify your game files.");
+    blackImage = new Util::Image::Image;
+    if (!Util::Image::LoadImageFromFile("Assets/GFX/Menu/Black.png", blackImage)) {
+        Util::Error::Exit("The texture \"Assets/GFX/Menu/Black.png\" failed to load. Ensure the file exists, or verify your game files.");
     }
 
-    optionsImage = new Util::Image::Image;
-    if (!Util::Image::LoadImageFromFile("Assets/GFX/Menu/Options.png", optionsImage)) {
-        Util::Error::Exit("The texture \"Assets/GFX/Menu/Options.png\" failed to load. Ensure the file exists, or verify your game files.");
-    }
-
-    quitImage = new Util::Image::Image;
-    if (!Util::Image::LoadImageFromFile("Assets/GFX/Menu/Quit.png", quitImage)) {
-        Util::Error::Exit("The texture \"Assets/GFX/Menu/Quit.png\" failed to load. Ensure the file exists, or verify your game files.");
+    scp173Image = new Util::Image::Image;
+    if (!Util::Image::LoadImageFromFile("Assets/GFX/Menu/SCP173.jpg", scp173Image)) {
+        Util::Error::Exit("The texture \"Assets/GFX/Menu/SCP173.png\" failed to load. Ensure the file exists, or verify your game files.");
     }
     
-    backImage = new Util::Image::Image;
-    if (!Util::Image::LoadImageFromFile("Assets/GFX/Menu/Back.png", backImage)) {
-        Util::Error::Exit("The texture \"Assets/GFX/Menu/Back.png\" failed to load. Ensure the file exists, or verify your game files.");
+    scpTextImage = new Util::Image::Image;
+    if (!Util::Image::LoadImageFromFile("Assets/GFX/Menu/SCPText.jpg", scpTextImage)) {
+        Util::Error::Exit("The texture \"Assets/GFX/Menu/SCPText.png\" failed to load. Ensure the file exists, or verify your game files.");
     }
 }
 
 void MainMenu::Render(GLFWwindow* window, GlobalGameState* gameState) {
-    int width, height;
-    glfwGetFramebufferSize(window, &width, &height);
+    try {
+        int width, height;
+        glfwGetFramebufferSize(window, &width, &height);
 
-    if (!mainMenuStarted) {
-        AudioEngine::PlaySoundByName("Assets/SFX/Music/Menu.ogg", AudioEngine::GetChannelGroup("Music"));
-        mainMenuStarted = true;
-    }
-
-    std::string verString = "SCP: CONTAINMENT BREACH REMASTERED V2.0.0 ALPHA";
-    ImGui::GetForegroundDrawList()->AddText(ImVec2(1, height - ImGui::CalcTextSize(verString.c_str()).y - 1), ImColor(255, 255, 255), std::string(std::to_string(ImGui::GetMousePos().x) + "x" + std::to_string(ImGui::GetMousePos().y)).c_str());
-    
-    // RENDER BACKGROUND
-    { 
-        ImGui::GetBackgroundDrawList()->AddImage(
-            reinterpret_cast<void*>(static_cast<intptr_t>(mainMenuBackgroundImage->TextureId)), ImVec2(0, 0),
-            ImVec2(mainMenuBackgroundImage->Width, mainMenuBackgroundImage->Height));
-    }
-
-    static int selectedMenu = 0;
-    
-    // MAIN
-    if (selectedMenu == 0) {
-        ImGui::SetNextWindowPos(ImVec2(144, 270));
-        ImGui::SetNextWindowSize(ImVec2(435, 480));
-        ImGui::PushStyleColor(ImGuiCol_WindowBg, ImVec4(0, 0, 0, 0));
-        ImGui::PushStyleColor(ImGuiCol_Border, ImVec4(0, 0, 0, 0));
-        ImGui::Begin("## MAIN-MENU-0", nullptr, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoMove);
-
-        static bool newGameHovered = false;
-        ImGui::Image(reinterpret_cast<void*>(newGameImage->TextureId), ImVec2(newGameImage->Width, newGameImage->Height), ImVec2(0, 0), ImVec2(1, 1), newGameHovered ? hovered : nonHovered);
-        if (ImGui::IsItemHovered()) {
-            newGameHovered = true;
-        } else {
-            newGameHovered = false;
-        }
-        if (ImGui::IsItemClicked()) {
-            selectedMenu = 1;
-            AudioEngine::PlaySoundByName("Assets\\SFX\\Splash\\Button.ogg", AudioEngine::GetChannelGroup("Game"));
+        if (!mainMenuStarted) {
+            AudioEngine::PlaySoundByName("Assets/SFX/Music/Menu.ogg", AudioEngine::GetChannelGroup("Music"));
+            mainMenuStarted = true;
         }
 
-        ImGui::Dummy(ImVec2(1, 20));
-
-        static bool loadGameHovered = false;
-        ImGui::Image(reinterpret_cast<void*>(loadGameImage->TextureId), ImVec2(loadGameImage->Width, loadGameImage->Height), ImVec2(0, 0), ImVec2(1, 1), loadGameHovered ? hovered : nonHovered);
-        if (ImGui::IsItemHovered()) {
-            loadGameHovered = true;
-        } else {
-            loadGameHovered = false;
-        }
-        if (ImGui::IsItemClicked()) {
-            selectedMenu = 2;
-            AudioEngine::PlaySoundByName("Assets\\SFX\\Splash\\Button.ogg", AudioEngine::GetChannelGroup("Game"));
-        }
-
-        ImGui::Dummy(ImVec2(1, 20));
+        std::string verString = "SCP: CONTAINMENT BREACH REMASTERED V2.0.0 ALPHA";
+        ImGui::GetForegroundDrawList()->AddText(ImVec2(1, height - ImGui::CalcTextSize(verString.c_str()).y - 1), ImColor(255, 255, 255), std::string(std::to_string(ImGui::GetMousePos().x) + "x" + std::to_string(ImGui::GetMousePos().y)).c_str());
         
-        static bool optionsHovered = false;
-        ImGui::Image(reinterpret_cast<void*>(optionsImage->TextureId), ImVec2(optionsImage->Width, optionsImage->Height), ImVec2(0, 0), ImVec2(1, 1), optionsHovered ? hovered : nonHovered);
-        if (ImGui::IsItemHovered()) {
-            optionsHovered = true;
-        } else {
-            optionsHovered = false;
-        }
-        if (ImGui::IsItemClicked()) {
-            selectedMenu = 3;
-            AudioEngine::PlaySoundByName("Assets\\SFX\\Splash\\Button.ogg", AudioEngine::GetChannelGroup("Game"));
+        // RENDER BACKGROUND
+        { 
+            ImGui::GetBackgroundDrawList()->AddImage(
+                reinterpret_cast<void*>(static_cast<intptr_t>(mainMenuBackgroundImage->TextureId)), ImVec2(0, 0),
+                ImVec2(mainMenuBackgroundImage->Width, mainMenuBackgroundImage->Height));
         }
 
-        ImGui::Dummy(ImVec2(1, 20));
+        // RENDER SCP TEXT
+        {
+            ImGui::PushStyleColor(ImGuiCol_WindowBg, ImVec4(0, 0, 0, 0));
+            ImGui::PushStyleColor(ImGuiCol_Border, ImVec4(0, 0, 0, 0));
+            ImGui::SetNextWindowPos(ImVec2(width / 2.0f, height), 0, ImVec2(0.5f, 1));
+            ImGui::SetNextWindowSize(ImVec2(scpTextImage->Width / 2.0f, scpTextImage->Height / 2.0f));
+            ImGui::Begin("## SCP-TEXT", nullptr, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoMove);
+            ImGui::SetCursorPos(ImVec2(0, 0));
+            ImGui::Image(reinterpret_cast<void*>(scpTextImage->TextureId), ImVec2(scpTextImage->Width / 2.0f, scpTextImage->Height / 2.0f));
+            ImGui::End();
+            ImGui::PopStyleColor(2);
+        }
 
-        static bool quitHovered = false;
-        ImGui::Image(reinterpret_cast<void*>(quitImage->TextureId), ImVec2(quitImage->Width, quitImage->Height), ImVec2(0, 0), ImVec2(1, 1), quitHovered ? hovered : nonHovered);
-        if (ImGui::IsItemHovered()) {
-            quitHovered = true;
-        } else {
-            quitHovered = false;
+        // RENDER SCP 173
+        {
+            ImGui::PushStyleColor(ImGuiCol_WindowBg, ImVec4(0, 0, 0, 0));
+            ImGui::PushStyleColor(ImGuiCol_Border, ImVec4(0, 0, 0, 0));
+            ImGui::SetNextWindowPos(ImVec2(width, height), 0, ImVec2(1, 1));
+            ImGui::SetNextWindowSize(ImVec2(scp173Image->Width, scp173Image->Height));
+            ImGui::Begin("## SCP-173", nullptr, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoMove);
+            ImGui::SetCursorPos(ImVec2(0, 0));
+            ImGui::Image(reinterpret_cast<void*>(scp173Image->TextureId), ImVec2(scp173Image->Width, scp173Image->Height));
+            ImGui::End();
+            ImGui::PopStyleColor(2);
         }
-        if (ImGui::IsItemClicked()) {
-            glfwSetWindowShouldClose(window, GLFW_TRUE);
-            AudioEngine::PlaySoundByName("Assets\\SFX\\Splash\\Button.ogg", AudioEngine::GetChannelGroup("Game"));
-        }
+
+        static int selectedMenu = 0;
         
-        ImGui::End();
-        ImGui::PopStyleColor(2);
-    } else if (selectedMenu == 1) {
-        ImGui::SetNextWindowPos(ImVec2(144, 270));
-        ImGui::SetNextWindowSize(ImVec2(610, 480));
-        ImGui::PushStyleColor(ImGuiCol_WindowBg, ImVec4(0, 0, 0, 0));
-        ImGui::PushStyleColor(ImGuiCol_Border, ImVec4(0, 0, 0, 0));
-        ImGui::Begin("## MAIN-MENU-1", nullptr, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoMove);
+        // MAIN
+        if (selectedMenu == 0) {
+            ImGui::SetNextWindowPos(ImVec2(144, 270));
+            ImGui::SetNextWindowSize(ImVec2(435, 480));
+            ImGui::PushStyleColor(ImGuiCol_WindowBg, ImVec4(0, 0, 0, 0));
+            ImGui::PushStyleColor(ImGuiCol_Border, ImVec4(0, 0, 0, 0));
+            ImGui::Begin("## MAIN-MENU-0", nullptr, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoMove);
 
-        ImGui::Image(reinterpret_cast<void*>(newGameImage->TextureId), ImVec2(newGameImage->Width, newGameImage->Height), ImVec2(0, 0), ImVec2(1, 1), nonHovered);
-        ImGui::SameLine(0, 5);
+            if (ImGui::ButtonCustom("NEW GAME", ImVec2(430, 75), whiteImage, blackImage)) {
+                selectedMenu = 1;
+                AudioEngine::PlaySoundByName("Assets\\SFX\\Splash\\Button.ogg", AudioEngine::GetChannelGroup("Game"));
+            }
 
-        static bool backHovered = false;
-        ImGui::Image(reinterpret_cast<void*>(backImage->TextureId), ImVec2(backImage->Width, backImage->Height), ImVec2(0, 0), ImVec2(1, 1), backHovered ? hovered : nonHovered);
-        if (ImGui::IsItemHovered()) {
-            backHovered = true;
-        } else {
-            backHovered = false;
+            ImGui::Dummy(ImVec2(1, 16));
+
+            if (ImGui::ButtonCustom("LOAD GAME", ImVec2(430, 75), whiteImage, blackImage)) {
+                selectedMenu = 2;
+                AudioEngine::PlaySoundByName("Assets\\SFX\\Splash\\Button.ogg", AudioEngine::GetChannelGroup("Game"));
+            }
+
+            ImGui::Dummy(ImVec2(1, 16));
+            
+            if (ImGui::ButtonCustom("OPTIONS", ImVec2(430, 75), whiteImage, blackImage)) {
+                selectedMenu = 3;
+                AudioEngine::PlaySoundByName("Assets\\SFX\\Splash\\Button.ogg", AudioEngine::GetChannelGroup("Game"));
+            }
+
+            ImGui::Dummy(ImVec2(1, 16));
+
+            if (ImGui::ButtonCustom("QUIT", ImVec2(430, 75), whiteImage, blackImage)) {
+                glfwSetWindowShouldClose(window, GLFW_TRUE);
+                AudioEngine::PlaySoundByName("Assets\\SFX\\Splash\\Button.ogg", AudioEngine::GetChannelGroup("Game"));
+            }
+            
+            ImGui::End();
+            ImGui::PopStyleColor(2);
+        } else if (selectedMenu == 1) {
+            ImGui::SetNextWindowPos(ImVec2(144, 270));
+            ImGui::SetNextWindowSize(ImVec2(610, 480));
+            ImGui::PushStyleColor(ImGuiCol_WindowBg, ImVec4(0, 0, 0, 0));
+            ImGui::PushStyleColor(ImGuiCol_Border, ImVec4(0, 0, 0, 0));
+            ImGui::Begin("## MAIN-MENU-1", nullptr, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoMove);
+
+            if (ImGui::ButtonCustom("NEW GAME", ImVec2(430, 75), whiteImage, blackImage)) {
+                selectedMenu = 1;
+            }
+            ImGui::SameLine();
+            if (ImGui::ButtonCustom("BACK", ImVec2(150, 75), whiteImage, blackImage, Localization::GetActiveLanguageCourierNew())) {
+                selectedMenu = 0;
+                AudioEngine::PlaySoundByName("Assets\\SFX\\Splash\\Button.ogg", AudioEngine::GetChannelGroup("Game"));
+            }
+
+            ImGui::Dummy(ImVec2(1, 15));
+
+            const ImVec2 currentPos = ImGui::GetWindowPos() + ImGui::GetCursorPos();
+            ImGui::BeginChildCustom("MAIN-MENU-1-OPTIONS", currentPos.x, currentPos.y, 590, 330, whiteImage, blackImage);
+
+            static std::string saveName = "untitled";
+            ImGui::InputTextCustom("Name:", &saveName, whiteImage, blackImage);
+            
+            static std::string seed = "9SOI25GEU";
+            ImGui::InputTextCustom("Map Seed:", &seed, whiteImage, blackImage);
+
+            ImGui::Dummy(ImVec2(1, 10));
+            
+            static bool introEnabled = true;
+            if (ImGui::CheckboxCustom("Enable intro sequence:", &introEnabled, whiteImage, blackImage)) {
+                AudioEngine::PlaySoundByName("Assets\\SFX\\Splash\\Button.ogg", AudioEngine::GetChannelGroup("Game"));
+            }
+            
+            ImGui::EndChildCustom();
+            
+            ImGui::End();
+            ImGui::PopStyleColor(2);
         }
-        if (ImGui::IsItemClicked()) {
-            AudioEngine::PlaySoundByName("Assets\\SFX\\Splash\\Button.ogg", AudioEngine::GetChannelGroup("Game"));
-            selectedMenu = 0;
-        }
-        
-        ImGui::End();
-        ImGui::PopStyleColor(2);
+    } catch (...) {
+        // IGNORE, CONTINUE TO NEXT FRAME
     }
 }
 
 void MainMenu::Free() {
     delete mainMenuBackgroundImage;
-    delete newGameImage;
-    delete loadGameImage;
-    delete optionsImage;
-    delete quitImage;
-    delete backImage;
+    delete blackImage;
+    delete whiteImage;
+    delete scp173Image;
+    delete scpTextImage;
 }
