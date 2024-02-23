309fc73f019e4c657b47b9ee8e4c18eefbd2cb35
Hunter Pollock <hunter@hnt8.net>
2024-02-23 18:45:18 -0500

    Translated everything, added random text popups

--- SCPCBR/MainMenu/MainMenu.cpp
+++ SCPCBR/MainMenu/MainMenu.cpp
@@ -1,27 +1,145 @@
 #include "MainMenu.h"
 
+#include <thread>
+
 #include "../Util/Util.h"
 #include "../AudioEngine/AudioEngine.h"
+#include "../Localization/Localization.h"
 
 #include <imgui/imgui.h>
 #include <imgui/imgui_custom.h>
 #include <FMOD/fmod.hpp>
 
-#include "archive_entry.h"
-#include "../Localization/Localization.h"
+#include <archive_entry.h>
 
-Util::Image::Image* mainMenuBackgroundImage;
-FMOD::Channel* mainMenuBackgroundMusic;
+namespace MainMenu {
+    int width, height;
+    
+    Util::Image::Image* mainMenuBackgroundImage;
+    FMOD::Channel* mainMenuBackgroundMusic;
 
-Util::Image::Image* whiteImage;
-Util::Image::Image* blackImage;
-Util::Image::Image* scp173Image;
-Util::Image::Image* scpTextImage;
+    Util::Image::Image* whiteImage;
+    Util::Image::Image* blackImage;
+    Util::Image::Image* scp173Image;
+    Util::Image::Image* scpTextImage;
 
-bool mainMenuStarted = false;
+    bool mainMenuStarted = false;
 
-ImVec4 nonHovered = ImVec4(1, 1, 1, 1);
-ImVec4 hovered = ImVec4(0.8f, 0.8f, 0.8f, 1);
+    ImVec4 nonHovered = ImVec4(1, 1, 1, 1);
+    ImVec4 hovered = ImVec4(0.8f, 0.8f, 0.8f, 1);
+
+    namespace PreTranslatedStrings {
+        std::string NewGame;
+        std::string LoadGame;
+        std::string Options;
+        std::string Quit;
+        std::string Back;
+        std::string SaveName;
+        std::string SaveSeed;
+        std::string IntroSeq;
+        std::string Difficulty;
+        std::string SafeDifficulty;
+        std::string SafeDifficultyDescription;
+        std::string EuclidDifficulty;
+        std::string EuclidDifficultyDescription;
+        std::string KeterDifficulty;
+        std::string KeterDifficultyDescription;
+        std::string ApollyonDifficulty;
+        std::string ApollyonDifficultyDescription;
+
+        std::string DontBlink;
+        std::string SecureContainProtect;
+        std::string HappyEndings;
+        std::string SometimesScream;
+        std::string Nil;
+        std::string No;
+        std::string BlackWhite;
+        std::string StoneCare;
+        std::string D9341;
+        std::string ControlsDoors;
+        std::string Random;
+        std::string TakenOver;
+        std::string Spiral;
+        std::string RealityDamage;
+    }
+}
+
+namespace RandomText {
+    bool ShouldShow = false;
+    std::string Text;
+    ImVec2 Position;
+    bool ShouldStopGenerating = false;
+}
+
+void RandomTextThread() {
+    Sleep(6000);
+    while (!RandomText::ShouldStopGenerating) {
+        switch (Util::Math::RandomInt(0, 13)) {
+            case 0: {
+                RandomText::Text = MainMenu::PreTranslatedStrings::DontBlink;
+                break;
+            }
+            case 1: {
+                RandomText::Text = MainMenu::PreTranslatedStrings::SecureContainProtect;
+                break;
+            }
+            case 2: {
+                RandomText::Text = MainMenu::PreTranslatedStrings::HappyEndings;
+                break;
+            }
+            case 3: {
+                RandomText::Text = MainMenu::PreTranslatedStrings::SometimesScream;
+                break;
+            }
+            case 4: {
+                RandomText::Text = MainMenu::PreTranslatedStrings::Nil;
+                break;
+            }
+            case 5: {
+                RandomText::Text = MainMenu::PreTranslatedStrings::No;
+                break;
+            }
+            case 6: {
+                RandomText::Text = MainMenu::PreTranslatedStrings::BlackWhite;
+                break;
+            }
+            case 7: {
+                RandomText::Text = MainMenu::PreTranslatedStrings::StoneCare;
+                break;
+            }
+            case 8: {
+                RandomText::Text = MainMenu::PreTranslatedStrings::D9341;
+                break;
+            }
+            case 9: {
+                RandomText::Text = MainMenu::PreTranslatedStrings::ControlsDoors;
+                break;
+            }
+            case 10: {
+                RandomText::Text = MainMenu::PreTranslatedStrings::Random;
+                break;
+            }
+            case 11: {
+                RandomText::Text = MainMenu::PreTranslatedStrings::TakenOver;
+                break;
+            }
+            case 12: {
+                RandomText::Text = MainMenu::PreTranslatedStrings::Spiral;
+                break;
+            }
+            case 13: {
+                RandomText::Text = MainMenu::PreTranslatedStrings::RealityDamage;
+                break;
+            }
+        }
+
+        RandomText::Position = ImVec2(Util::Math::RandomInt(MainMenu::width / 2.0f - 300, MainMenu::width / 2.0f + 300), Util::Math::RandomInt(MainMenu::height / 2.0f - 300, MainMenu::height / 2.0f + 300));
+        RandomText::ShouldShow = true;
+        Sleep(1000);
+        RandomText::ShouldShow = false;
+        Sleep(Util::Math::RandomInt(10000, 45000));
+    }
+}
 
 void MainMenu::Init() {
     mainMenuBackgroundImage = new Util::Image::Image;
@@ -48,11 +166,45 @@ void MainMenu::Init() {
     if (!Util::Image::LoadImageFromFile("Assets/GFX/Menu/SCPText.jpg", scpTextImage)) {
         Util::Error::Exit("The texture \"Assets/GFX/Menu/SCPText.png\" failed to load. Ensure the file exists, or verify your game files.");
     }
+
+    PreTranslatedStrings::NewGame = Localization::GetTranslatedKey("MainMenu", "newgame");
+    PreTranslatedStrings::LoadGame = Localization::GetTranslatedKey("MainMenu", "loadgame");
+    PreTranslatedStrings::Options = Localization::GetTranslatedKey("MainMenu", "options");
+    PreTranslatedStrings::Quit = Localization::GetTranslatedKey("MainMenu", "quit");
+    PreTranslatedStrings::Back = Localization::GetTranslatedKey("MainMenu", "back");
+    PreTranslatedStrings::SaveName = Localization::GetTranslatedKey("MainMenu", "savename");
+    PreTranslatedStrings::SaveSeed = Localization::GetTranslatedKey("MainMenu", "saveseed");
+    PreTranslatedStrings::IntroSeq = Localization::GetTranslatedKey("MainMenu", "introseq");
+    PreTranslatedStrings::Difficulty = Localization::GetTranslatedKey("MainMenu", "difficulty");
+    PreTranslatedStrings::SafeDifficulty = Localization::GetTranslatedKey("MainMenu", "safe");
+    PreTranslatedStrings::SafeDifficultyDescription = Localization::GetTranslatedKey("MainMenu", "safedescription");
+    PreTranslatedStrings::EuclidDifficulty = Localization::GetTranslatedKey("MainMenu", "euclid");
+    PreTranslatedStrings::EuclidDifficultyDescription = Localization::GetTranslatedKey("MainMenu", "eucliddescription");
+    PreTranslatedStrings::KeterDifficulty = Localization::GetTranslatedKey("MainMenu", "keter");
+    PreTranslatedStrings::KeterDifficultyDescription = Localization::GetTranslatedKey("MainMenu", "keterdescription");
+    PreTranslatedStrings::ApollyonDifficulty = Localization::GetTranslatedKey("MainMenu", "apollyon");
+    PreTranslatedStrings::ApollyonDifficultyDescription = Localization::GetTranslatedKey("MainMenu", "apollyondescription");
+
+    PreTranslatedStrings::DontBlink = Localization::GetTranslatedKey("MainMenu", "dontblink");
+    PreTranslatedStrings::SecureContainProtect = Localization::GetTranslatedKey("MainMenu", "securecontainprotect");
+    PreTranslatedStrings::HappyEndings = Localization::GetTranslatedKey("MainMenu", "happyendings");
+    PreTranslatedStrings::SometimesScream = Localization::GetTranslatedKey("MainMenu", "sometimesscream");
+    PreTranslatedStrings::Nil = Localization::GetTranslatedKey("MainMenu", "nil");
+    PreTranslatedStrings::No = Localization::GetTranslatedKey("MainMenu", "no");
+    PreTranslatedStrings::BlackWhite = Localization::GetTranslatedKey("MainMenu", "blackwhite");
+    PreTranslatedStrings::StoneCare = Localization::GetTranslatedKey("MainMenu", "stonecare");
+    PreTranslatedStrings::D9341 = Localization::GetTranslatedKey("MainMenu", "d9341");
+    PreTranslatedStrings::ControlsDoors = Localization::GetTranslatedKey("MainMenu", "controlsdoors");
+    PreTranslatedStrings::Random = Localization::GetTranslatedKey("MainMenu", "random");
+    PreTranslatedStrings::TakenOver = Localization::GetTranslatedKey("MainMenu", "takenover");
+    PreTranslatedStrings::Spiral = Localization::GetTranslatedKey("MainMenu", "spiral");
+    PreTranslatedStrings::RealityDamage = Localization::GetTranslatedKey("MainMenu", "realitydamage");
+
+    std::thread(RandomTextThread).detach();
 }
 
 void MainMenu::Render(GLFWwindow* window, GlobalGameState* gameState) {
     try {
-        int width, height;
         glfwGetFramebufferSize(window, &width, &height);
 
         if (!mainMenuStarted) {
@@ -83,8 +235,14 @@ void MainMenu::Render(GLFWwindow* window, GlobalGameState* gameState) {
             ImGui::PopStyleColor(2);
         }
 
+        // RENDER RANDOM TEXT
+        if (RandomText::ShouldShow) {
+            ImVec2 newPos = RandomText::Position + ImVec2(Util::Math::RandomInt(-5, 5), Util::Math::RandomInt(-5, 5));
+            ImGui::GetForegroundDrawList()->AddText(newPos, ImColor(100, 100, 100), RandomText::Text.c_str());
+        }
+
         // RENDER SCP 173
-        {
+        if (Util::Math::RandomFloat(0.0f, 1.0f) <= 0.3f) {
             ImGui::PushStyleColor(ImGuiCol_WindowBg, ImVec4(0, 0, 0, 0));
             ImGui::PushStyleColor(ImGuiCol_Border, ImVec4(0, 0, 0, 0));
             ImGui::SetNextWindowPos(ImVec2(width, height), 0, ImVec2(1, 1));
@@ -106,28 +264,29 @@ void MainMenu::Render(GLFWwindow* window, GlobalGameState* gameState) {
             ImGui::PushStyleColor(ImGuiCol_Border, ImVec4(0, 0, 0, 0));
             ImGui::Begin("## MAIN-MENU-0", nullptr, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoMove);
 
-            if (ImGui::ButtonCustom("NEW GAME", ImVec2(430, 75), whiteImage, blackImage)) {
+            if (ImGui::ButtonCustom(PreTranslatedStrings::NewGame, ImVec2(430, 75), whiteImage, blackImage)) {
                 selectedMenu = 1;
                 AudioEngine::PlaySoundByName("Assets\\SFX\\Splash\\Button.ogg", AudioEngine::GetChannelGroup("Game"));
             }
 
             ImGui::Dummy(ImVec2(1, 16));
 
-            if (ImGui::ButtonCustom("LOAD GAME", ImVec2(430, 75), whiteImage, blackImage)) {
+            if (ImGui::ButtonCustom(PreTranslatedStrings::LoadGame, ImVec2(430, 75), whiteImage, blackImage)) {
                 selectedMenu = 2;
                 AudioEngine::PlaySoundByName("Assets\\SFX\\Splash\\Button.ogg", AudioEngine::GetChannelGroup("Game"));
             }
 
             ImGui::Dummy(ImVec2(1, 16));
             
-            if (ImGui::ButtonCustom("OPTIONS", ImVec2(430, 75), whiteImage, blackImage)) {
+            if (ImGui::ButtonCustom(PreTranslatedStrings::Options, ImVec2(430, 75), whiteImage, blackImage)) {
                 selectedMenu = 3;
                 AudioEngine::PlaySoundByName("Assets\\SFX\\Splash\\Button.ogg", AudioEngine::GetChannelGroup("Game"));
             }
 
             ImGui::Dummy(ImVec2(1, 16));
 
-            if (ImGui::ButtonCustom("QUIT", ImVec2(430, 75), whiteImage, blackImage)) {
+            if (ImGui::ButtonCustom(PreTranslatedStrings::Quit, ImVec2(430, 75), whiteImage, blackImage)) {
+                RandomText::ShouldStopGenerating = true;
                 glfwSetWindowShouldClose(window, GLFW_TRUE);
                 AudioEngine::PlaySoundByName("Assets\\SFX\\Splash\\Button.ogg", AudioEngine::GetChannelGroup("Game"));
             }
@@ -141,32 +300,91 @@ void MainMenu::Render(GLFWwindow* window, GlobalGameState* gameState) {
             ImGui::PushStyleColor(ImGuiCol_Border, ImVec4(0, 0, 0, 0));
             ImGui::Begin("## MAIN-MENU-1", nullptr, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoMove);
 
-            if (ImGui::ButtonCustom("NEW GAME", ImVec2(430, 75), whiteImage, blackImage)) {
+            if (ImGui::ButtonCustom(PreTranslatedStrings::NewGame, ImVec2(430, 75), whiteImage, blackImage)) {
                 selectedMenu = 1;
             }
             ImGui::SameLine();
-            if (ImGui::ButtonCustom("BACK", ImVec2(150, 75), whiteImage, blackImage, Localization::GetActiveLanguageCourierNew())) {
+            if (ImGui::ButtonCustom(PreTranslatedStrings::Back, ImVec2(150, 75), whiteImage, blackImage, Localization::GetActiveLanguageCourierNew())) {
                 selectedMenu = 0;
                 AudioEngine::PlaySoundByName("Assets\\SFX\\Splash\\Button.ogg", AudioEngine::GetChannelGroup("Game"));
             }
 
-            ImGui::Dummy(ImVec2(1, 15));
+            ImGui::Dummy(ImVec2(1, 1));
 
             const ImVec2 currentPos = ImGui::GetWindowPos() + ImGui::GetCursorPos();
-            ImGui::BeginChildCustom("MAIN-MENU-1-OPTIONS", currentPos.x, currentPos.y, 590, 330, whiteImage, blackImage);
+            ImGui::Dummy(ImVec2(590, 315));
+            ImGui::BeginChildCustom("MAIN-MENU-1-OPTIONS", currentPos.x, currentPos.y, 590, 315, whiteImage, blackImage);
 
             static std::string saveName = "untitled";
-            ImGui::InputTextCustom("Name:", &saveName, whiteImage, blackImage);
+            ImGui::InputTextCustom(PreTranslatedStrings::SaveName.c_str(), &saveName, whiteImage, blackImage);
             
             static std::string seed = "9SOI25GEU";
-            ImGui::InputTextCustom("Map Seed:", &seed, whiteImage, blackImage);
+            ImGui::InputTextCustom(PreTranslatedStrings::SaveSeed.c_str(), &seed, whiteImage, blackImage);
 
             ImGui::Dummy(ImVec2(1, 10));
             
             static bool introEnabled = true;
-            if (ImGui::CheckboxCustom("Enable intro sequence:", &introEnabled, whiteImage, blackImage)) {
+            if (ImGui::CheckboxCustom(PreTranslatedStrings::IntroSeq.c_str(), &introEnabled, whiteImage, blackImage)) {
                 AudioEngine::PlaySoundByName("Assets\\SFX\\Splash\\Button.ogg", AudioEngine::GetChannelGroup("Game"));
             }
+
+            ImGui::Dummy(ImVec2(1, 10));
+
+            ImGui::Text("%s", PreTranslatedStrings::Difficulty.c_str());
+
+            ImGui::Dummy(ImVec2(1, 6));
+
+            static bool safe = true;
+            static bool euclid = false;
+            static bool keter = false;
+            static bool apollyon = false;
+            static const char* description = PreTranslatedStrings::SafeDifficultyDescription.c_str();
+
+            ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(96 / 255.0f, 117 / 255.0f, 46 / 255.0f, 1));
+            if (ImGui::CheckboxCustom(PreTranslatedStrings::SafeDifficulty.c_str(), &safe, whiteImage, blackImage, 110)) {
+                euclid = false;
+                keter = false;
+                apollyon = false;
+                description = PreTranslatedStrings::SafeDifficultyDescription.c_str();
+                AudioEngine::PlaySoundByName("Assets\\SFX\\Splash\\Button.ogg", AudioEngine::GetChannelGroup("Game"));
+            }
+            ImGui::PopStyleColor();
+
+            ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(152 / 255.0f, 152 / 255.0f, 10 / 255.0f, 1));
+            if (ImGui::CheckboxCustom(PreTranslatedStrings::EuclidDifficulty.c_str(), &euclid, whiteImage, blackImage, 110)) {
+                safe = false;
+                keter = false;
+                apollyon = false;
+                description = PreTranslatedStrings::EuclidDifficultyDescription.c_str();
+                AudioEngine::PlaySoundByName("Assets\\SFX\\Splash\\Button.ogg", AudioEngine::GetChannelGroup("Game"));
+            }
+            ImGui::PopStyleColor();
+
+            ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(152 / 255.0f, 11 / 255.0f, 10 / 255.0f, 1));
+            if (ImGui::CheckboxCustom(PreTranslatedStrings::KeterDifficulty.c_str(), &keter, whiteImage, blackImage, 110)) {
+                safe = false;
+                euclid = false;
+                apollyon = false;
+                description = PreTranslatedStrings::KeterDifficultyDescription.c_str();
+                AudioEngine::PlaySoundByName("Assets\\SFX\\Splash\\Button.ogg", AudioEngine::GetChannelGroup("Game"));
+            }
+            ImGui::PopStyleColor();
+
+            ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(80 / 255.0f, 79 / 255.0f, 79 / 255.0f, 1));
+            if (ImGui::CheckboxCustom(PreTranslatedStrings::ApollyonDifficulty.c_str(), &apollyon, whiteImage, blackImage, 110)) {
+                safe = false;
+                euclid = false;
+                keter = false;
+                description = PreTranslatedStrings::ApollyonDifficultyDescription.c_str();
+                AudioEngine::PlaySoundByName("Assets\\SFX\\Splash\\Button.ogg", AudioEngine::GetChannelGroup("Game"));
+            }
+            ImGui::PopStyleColor();
+
+            ImGui::BeginChildCustom("DIFFICULTY-DESCRIPTION", 316, 549, 399, 103, whiteImage, blackImage, 1.0f);
+            ImGui::PushFont(Localization::GetActiveLanguageCourierNewSmall());
+            ImGui::TextWrapped("%s", description);
+            ImGui::PopFont();
+            ImGui::EndChildCustom();
             
             ImGui::EndChildCustom();
             
