15b250b1715671183e95b04eba251c64128d9c85
hlpdev <hunterpollock2006@gmail.com>
2024-09-07 23:02:22 -0400

    Added bgfx for 3d rendering

---
+++ SCPCBR/Dependencies/src/version.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright 2011-2024 Branimir Karadzic. All rights reserved.
+ * License: https://github.com/bkaradzic/bgfx/blob/master/LICENSE
+ */
+
+/*
+ *
+ * AUTO GENERATED! DO NOT EDIT!
+ *
+ */
+
+#define BGFX_REV_NUMBER 8775
+#define BGFX_REV_SHA1   "a476c5b9a42d3779af59a0099d4d222fa8898d36"
